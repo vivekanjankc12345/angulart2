bfc048cdfae5160a88834831b21d4c1509e2de92
Vivek Anjan <vivekanjankc12345@gmail.com>
2023-07-13 23:05:36 +0530

    done

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+  count: number = 0;
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  }
+}
